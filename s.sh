a6cdbf19b046381e68bd9f0b73633fc9c57cc879
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-03-22 18:04:48 +0530

    added ls in s.sh file

--- s.sh
+++ s.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 python3 --version
 
-
-ssh -o StrictHostKeyChecking=no -tt -i /tmp/.schematics/IBM/tf_data_path/id_rsa root@${bastion_ip} /bin/bash <<'EOT'
-echo "These commands will be run on: $( uname -a )"
-echo "They are executed by: $( whoami )"
 ls /root/
 cat /root/.ssh/authorized_keys
-exit
-EOT
-
