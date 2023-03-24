49350bb5f726be2da4bee5646c7c4ffe138481c2
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-03-24 13:23:08 +0530

    added main

--- man.tf
+++ man.tf
@@ -71,6 +71,78 @@
 # #   value = tls_private_key.generate_ssh_key.private_key_pem
 # #   sensitive = true
 # # }
+# ####################################################################################
+
+# variable "ssh_source_cidr_override" {
+#   type        = list
+#   description = "Override CIDR range that is allowed to ssh to the bastion"
+#   default     = ["0.0.0.0/0"]
+# }
+# variable "bastion_cidr" {
+#   description = "Complete CIDR range across all three zones for bastion host subnets"
+#   default     = "172.22.192.0/20"
+# }
+# variable "frontend_cidr" {
+#   description = "Complete CIDR range across all three zones for frontend subnets"
+#   default     = "172.16.0.0/20"
+# }
+
+
+# #data "external" "env" { program = ["jq", "-n", "env"] }
+# locals {
+#   region = "us-south"
+#   geo    = substr(local.region, 0, 2)
+#   schematics_ssh_access_map = {
+#     us = ["169.44.0.0/14", "169.60.0.0/14"],
+#     eu = ["158.175.0.0/16","158.176.0.0/15","141.125.75.80/28","161.156.139.192/28","149.81.103.128/28"],
+#   }
+#   schematics_ssh_access = lookup(local.schematics_ssh_access_map, local.geo, ["0.0.0.0/0"])
+#   bastion_ingress_cidr  = var.ssh_source_cidr_override[0] != "0.0.0.0/0" ? var.ssh_source_cidr_override : local.schematics_ssh_access
+# }
+
+# # locals {
+# #   # bastion_cidr_blocks  = [cidrsubnet(var.bastion_cidr, 4, 0), cidrsubnet(var.bastion_cidr, 4, 2), cidrsubnet(var.bastion_cidr, 4, 4)]
+# #   frontend_cidr_blocks = [cidrsubnet(var.frontend_cidr, 4, 0), cidrsubnet(var.frontend_cidr, 4, 2), cidrsubnet(var.frontend_cidr, 4, 4)]
+# # }
+
+# # Create single zone bastion
+# module "bastion" {
+#   source                   = "./bastionmodule"
+#   ibm_region               = "us-south"
+#   bastion_count            = 1
+#   unique_id                = "jay-lsf-scale-vpc-1"
+#   ibm_is_vpc_id            = "r006-229da5c6-4f1a-44b9-951d-21a8fdb95aa3"
+#   ibm_is_resource_group_id = "2cd68a3483634533b41a8993159c27e8"
+#   bastion_cidr             = var.bastion_cidr
+#   ssh_source_cidr_blocks   = local.bastion_ingress_cidr
+#   destination_cidr_blocks  = [var.frontend_cidr]
+#   destination_sgs          = [module.frontend.security_group_id]
+#   # destination_sg          = [module.frontend.security_group_id, module.backend.security_group_id]
+#   # vsi_profile             = "cx2-2x4"
+#   # image_name              = "ibm-centos-7-6-minimal-amd64-1"
+#   ssh_key_id                = [ibm_is_ssh_key.jay-sssh-key.id]
+#   user_data_public          = data.template_file.login_user_data.rendered
+#   user_data_private         = data.template_file.login_user_data_private.rendered
+#   sg                        = ibm_is_security_group.login_sg.id
+# }
+
+# module "frontend" {
+#   source                   = "./frontendmodule"
+#   ibm_region               = "us-south"
+#   unique_id                = "jay-lsf-scale-vpc-1"
+#   ibm_is_vpc_id            = "r006-229da5c6-4f1a-44b9-951d-21a8fdb95aa3"
+#   ibm_is_resource_group_id = "2cd68a3483634533b41a8993159c27e8"
+#   frontend_count           = 1
+#   profile                  = "cx2-2x4"
+#   ibm_is_image_id          = "r006-7ca7884c-c797-468e-a565-5789102aedc6"
+#   ibm_is_ssh_key_id        = [ibm_is_ssh_key.jay-sssh-key.id]
+#   subnet_ids               = "0737-8b8cccd9-10a2-475e-945b-7ee62375e384"
+#   bastion_remote_sg_id     = module.bastion.security_group_id
+#   bastion_subnet_CIDR      = var.bastion_cidr
+#   #pub_repo_egress_cidr     = local.pub_repo_egress_cidr
+# }
+
+# ####################################################################################
 # #===================================================================================
 # resource "ibm_is_security_group" "login_sg" {
 #   name           = "jay-schematics-login-sg"
@@ -185,13 +257,19 @@
 # echo "${tls_private_key.generate_ssh_key.public_key_openssh}" >> ~/.ssh/authorized_keys
 # EOF
 # }
+# data "template_file" "login_user_data_private" {
+#   template = <<EOF
+# #!/usr/bin/env bash
+# echo "${tls_private_key.generate_ssh_key.private_key_pem}" >> ~/.ssh/id_rsa
+# EOF
+# }
 # resource "ibm_is_instance" "login" {
 #   name           = "jay-schematics-check"
 #   image          = "r006-7ca7884c-c797-468e-a565-5789102aedc6"
 #   profile        = "bx2-2x8"
 #   zone           = "us-south-3"
 #   keys           = [ibm_is_ssh_key.jay-sssh-key.id]
-#   user_data      = data.template_file.login_user_data.rendered
+#   user_data      = "${data.template_file.login_user_data.rendered} ${data.template_file.login_user_data_private.rendered}"
 #   vpc            = "r006-229da5c6-4f1a-44b9-951d-21a8fdb95aa3"
 #   resource_group = "2cd68a3483634533b41a8993159c27e8"
 
@@ -199,7 +277,7 @@
 #   primary_network_interface {
 #     name            = "eth0"
 #     subnet          = "0737-8b8cccd9-10a2-475e-945b-7ee62375e384"
-#     security_groups = [ibm_is_security_group.login_sg.id]
+#     security_groups = [ibm_is_security_group.login_sg.id, module.frontend.security_group_id]
 #   }
 # }
 # #===================================================================================
@@ -251,7 +329,7 @@
 #     command     = "/bin/bash ${path.module}/script.sh"
 
 #     environment = {
-#       "bastion_ip" : ibm_is_floating_ip.login_fip.address
+#       "bastion_ip" : module.bastion.floating_ip_address #ibm_is_floating_ip.login_fip.address
 #       "target_ip"  : ibm_is_instance.target-node.primary_network_interface.0.primary_ip.0.address
 #       "ini_file"   : "${path.module}/inventory.ini"
 #       #"key_path"   : format("%s/%s", var.tf_data_path, "id_rsa")
@@ -323,7 +401,7 @@
 #   primary_network_interface {
 #     name            = "eth0"
 #     subnet          = "0737-3695813f-6c12-4afb-b419-4c677189a4e9"
-#     security_groups = [ibm_is_security_group.schematics_sg.id]
+#     security_groups = [ibm_is_security_group.schematics_sg.id, module.bastion.security_group_id]
 #   }
 # }
 
