d770d88163af5b616514fe500a60ec1c5921c67f
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-03-27 15:08:34 +0530

    /etc/ssh/ssh_config

--- script.sh
+++ script.sh
@@ -21,6 +21,8 @@ ls /etc/ssh/ssh_config.d
 echo "################################################"
 cat /etc/ssh/ssh_config
 echo "################################################"
+echo "PasswordAuthentication no" >> /etc/ssh/ssh_config
+
 # ssh -o StrictHostKeyChecking=no -tt -i /tmp/.schematics/IBM/tf_data_path/id_rsa -J root@${bastion_ip} root@${target_ip} /bin/bash <<'EOT'
 # echo "These commands will be run on: $( uname -a )"
 # echo "They are executed by: $( whoami )"
