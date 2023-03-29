6c6283eeaa1c53a143e7a0f6b5103032afd41fe8
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-03-29 12:53:17 +0530

    added bstion

--- script.sh
+++ script.sh
@@ -6,15 +6,15 @@ then
     echo "${ini_file} file cleaned"
 fi
 echo "[scale_nodes]" >> ${ini_file}
-echo "${target_ip}" >> ${ini_file}
+echo "${bastion_ip}" >> ${ini_file}
 
-proxy_command="ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand=\"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${bastion_ip} -i /tmp/.schematics/IBM/tf_data_path/id_rsa\"'"
+#proxy_command="ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand=\"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p root@${bastion_ip} -i /tmp/.schematics/IBM/tf_data_path/id_rsa\"'"
 
-echo "[scale_nodes:vars]" >> ${ini_file}
-echo "remote_tmp = /tmp/ansible-$USER"
-echo ${proxy_command} >> ${ini_file}
+#echo "[scale_nodes:vars]" >> ${ini_file}
+#echo "remote_tmp = /tmp/ansible-$USER"
+#echo ${proxy_command} >> ${ini_file}
 
-echo "inventory file writte"
+echo "inventory file written"
 echo "################################################"
 ls /etc/ssh 
 echo "################################################"
