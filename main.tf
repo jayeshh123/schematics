cec4011fc99d6e9d3a629460627748ab3aef6176
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-03-30 09:28:35 +0530

    commented sg

--- main.tf
+++ main.tf
@@ -194,115 +194,115 @@ resource "ibm_is_security_group_rule" "login_egress_udp_rhsm" {
 # }
 
 #===================================================================================
-variable "ssh_source_cidr_override" {
-  type        = list
-  description = "Override CIDR range that is allowed to ssh to the bastion"
-  default     = ["0.0.0.0/0"]
-}
+# variable "ssh_source_cidr_override" {
+#   type        = list
+#   description = "Override CIDR range that is allowed to ssh to the bastion"
+#   default     = ["0.0.0.0/0"]
+# }
 
-locals {
-  region = "us-south"
-  geo    = substr(local.region, 0, 2)
-  schematics_ssh_access_map = {
-    us = ["169.44.0.0/14", "169.60.0.0/14"],
-    eu = ["158.175.0.0/16","158.176.0.0/15","141.125.75.80/28","161.156.139.192/28","149.81.103.128/28"],
-  }
-  schematics_ssh_access = lookup(local.schematics_ssh_access_map, local.geo, ["0.0.0.0/0"])
-  bastion_ingress_cidr  = var.ssh_source_cidr_override[0] != "0.0.0.0/0" ? var.ssh_source_cidr_override : local.schematics_ssh_access
-}
+# locals {
+#   region = "us-south"
+#   geo    = substr(local.region, 0, 2)
+#   schematics_ssh_access_map = {
+#     us = ["169.44.0.0/14", "169.60.0.0/14"],
+#     eu = ["158.175.0.0/16","158.176.0.0/15","141.125.75.80/28","161.156.139.192/28","149.81.103.128/28"],
+#   }
+#   schematics_ssh_access = lookup(local.schematics_ssh_access_map, local.geo, ["0.0.0.0/0"])
+#   bastion_ingress_cidr  = var.ssh_source_cidr_override[0] != "0.0.0.0/0" ? var.ssh_source_cidr_override : local.schematics_ssh_access
+# }
 
-#===================================================================================
-# this is the SG applied to the bastion instance
-variable "destination_sgs" {
-  description = "Destination Security Groups in VPC"
-  default     = []
-}
-
-
-# resource "ibm_is_security_group" "bastion" {
-#   name           = "jay-bastion-sg"
-#   vpc            = "r006-229da5c6-4f1a-44b9-951d-21a8fdb95aa3"
-#   resource_group = var.ibm_is_resource_group_id
+# #===================================================================================
+# # this is the SG applied to the bastion instance
+# variable "destination_sgs" {
+#   description = "Destination Security Groups in VPC"
+#   default     = []
 # }
 
 
-locals {
-  sg_keys = ["direction", "remote", "type", "port_min", "port_max"]
-
-  # base rules for maintenance repo's, DNS 
-  sg_baserules = [
-    ["outbound", "161.26.0.0/16", "udp", 53, 53],
-    ["outbound", "161.26.0.0/16", "tcp", 80, 80],
-    ["outbound", "161.26.0.0/16", "tcp", 443, 443],
-  ]
-
-  sg_sourcerules = [
-    for entry in local.bastion_ingress_cidr :
-    ["inbound", entry, "tcp", 22, 22]
-  ]
-
-  sg_destrules = [
-    for entry in var.destination_sgs :
-    ["outbound", entry, "tcp", 22, 22]
-  ]
+# # resource "ibm_is_security_group" "bastion" {
+# #   name           = "jay-bastion-sg"
+# #   vpc            = "r006-229da5c6-4f1a-44b9-951d-21a8fdb95aa3"
+# #   resource_group = var.ibm_is_resource_group_id
+# # }
 
 
-  #concatinate all sources of rules
-  sg_rules = concat(local.sg_sourcerules, local.sg_destrules, local.sg_baserules)
-  sg_mappedrules = [
-    for entry in local.sg_rules :
-    merge(zipmap(local.sg_keys, entry))
-  ]
-}
+# locals {
+#   sg_keys = ["direction", "remote", "type", "port_min", "port_max"]
+
+#   # base rules for maintenance repo's, DNS 
+#   sg_baserules = [
+#     ["outbound", "161.26.0.0/16", "udp", 53, 53],
+#     ["outbound", "161.26.0.0/16", "tcp", 80, 80],
+#     ["outbound", "161.26.0.0/16", "tcp", 443, 443],
+#   ]
+
+#   sg_sourcerules = [
+#     for entry in local.bastion_ingress_cidr :
+#     ["inbound", entry, "tcp", 22, 22]
+#   ]
+
+#   sg_destrules = [
+#     for entry in var.destination_sgs :
+#     ["outbound", entry, "tcp", 22, 22]
+#   ]
 
 
-output "list_sg_rules" {
-  value = local.sg_mappedrules
-}
+#   #concatinate all sources of rules
+#   sg_rules = concat(local.sg_sourcerules, local.sg_destrules, local.sg_baserules)
+#   sg_mappedrules = [
+#     for entry in local.sg_rules :
+#     merge(zipmap(local.sg_keys, entry))
+#   ]
+# }
 
-resource "ibm_is_security_group_rule" "bastion_access" {
-  count     = length(local.sg_mappedrules)
-  group     = ibm_is_security_group.login_sg.id
-  direction = (local.sg_mappedrules[count.index]).direction
-  remote    = (local.sg_mappedrules[count.index]).remote
-  dynamic "tcp" {
-    for_each = local.sg_mappedrules[count.index].type == "tcp" ? [
-      {
-        port_max = local.sg_mappedrules[count.index].port_max
-        port_min = local.sg_mappedrules[count.index].port_min
-      }
-    ] : []
-    content {
-      port_max = tcp.value.port_max
-      port_min = tcp.value.port_min
 
-    }
-  }
-  dynamic "udp" {
-    for_each = local.sg_mappedrules[count.index].type == "udp" ? [
-      {
-        port_max = local.sg_mappedrules[count.index].port_max
-        port_min = local.sg_mappedrules[count.index].port_min
-      }
-    ] : []
-    content {
-      port_max = udp.value.port_max
-      port_min = udp.value.port_min
-    }
-  }
-  dynamic "icmp" {
-    for_each = local.sg_mappedrules[count.index].type == "icmp" ? [
-      {
-        type = local.sg_mappedrules[count.index].port_max
-        code = local.sg_mappedrules[count.index].port_min
-      }
-    ] : []
-    content {
-      type = icmp.value.type
-      code = icmp.value.code
-    }
-  }
-}
+# output "list_sg_rules" {
+#   value = local.sg_mappedrules
+# }
+
+# resource "ibm_is_security_group_rule" "bastion_access" {
+#   count     = length(local.sg_mappedrules)
+#   group     = ibm_is_security_group.login_sg.id
+#   direction = (local.sg_mappedrules[count.index]).direction
+#   remote    = (local.sg_mappedrules[count.index]).remote
+#   dynamic "tcp" {
+#     for_each = local.sg_mappedrules[count.index].type == "tcp" ? [
+#       {
+#         port_max = local.sg_mappedrules[count.index].port_max
+#         port_min = local.sg_mappedrules[count.index].port_min
+#       }
+#     ] : []
+#     content {
+#       port_max = tcp.value.port_max
+#       port_min = tcp.value.port_min
+
+#     }
+#   }
+#   dynamic "udp" {
+#     for_each = local.sg_mappedrules[count.index].type == "udp" ? [
+#       {
+#         port_max = local.sg_mappedrules[count.index].port_max
+#         port_min = local.sg_mappedrules[count.index].port_min
+#       }
+#     ] : []
+#     content {
+#       port_max = udp.value.port_max
+#       port_min = udp.value.port_min
+#     }
+#   }
+#   dynamic "icmp" {
+#     for_each = local.sg_mappedrules[count.index].type == "icmp" ? [
+#       {
+#         type = local.sg_mappedrules[count.index].port_max
+#         code = local.sg_mappedrules[count.index].port_min
+#       }
+#     ] : []
+#     content {
+#       type = icmp.value.type
+#       code = icmp.value.code
+#     }
+#   }
+# }
 
 #===================================================================================
 resource "ibm_is_floating_ip" "login_fip" {
